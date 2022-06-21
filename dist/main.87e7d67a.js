3ae89b7245e3a3c551e21375c4d758febf009531
Mianxiong <757134184@qq.com>
2022-06-21 14:06:20 +0800

    bug修复

--- dist/main.87e7d67a.js
+++ dist/main.87e7d67a.js
@@ -103,9 +103,86 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({"main.js":[function(require,module,exports) {
+})({"css.js":[function(require,module,exports) {
+"use strict";
 
-},{}],"C:\\Users\\hmx\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var string = "\n.skin * {\n    box-sizing: border-box;\n    margin: 0;\n    padding: 0;\n  }\n  .skin *::before,\n  .skin *::after {\n    box-sizing: border-box;\n  }\n  .skin {\n    background: #ffe600;\n    /* \u5360\u6EE1\u5168\u5C4F */\n    min-height: 100vh;\n    position: relative;\n  }\n  .nose {\n    /* \u4E09\u89D2\u5F62\u7684\u9AD8\u5EA6 */\n    border: 10px solid black;\n    border-color: black transparent transparent transparent;\n    border-bottom: none;\n    width: 0px;\n    height: 0px;\n    position: absolute;\n    left: 50%;\n    top: 145px;\n    transform: translateX(-50%);\n    z-index: 10;\n  }\n  @keyframes wave {\n      0% {\n          transform: rotate(0deg);\n      }\n      33% {\n          transform: rotate(5deg);\n      }\n      66% {\n          transform: rotate(-5deg);\n      }\n      100% {\n          transform: rotate(0deg);\n      }\n  }\n  .nose:hover{\n      transform-origin: center bottom;\n      animation: wave 300ms infinite linear;\n  }\n  .yuan {\n    position: absolute;\n    width: 20px;\n    height: 6px;\n    /* border: 1px solid green; */\n    top: -16px;\n    transform: translate(-50%);\n    border-radius: 10px 10px 0 0;\n    background: black;\n  }\n  .eye {\n    border: 2px solid #000;\n    width: 64px;\n    height: 64px;\n    position: absolute;\n    left: 50%;\n    top: 100px;\n    margin-left: -32px;\n    background: #2e2e2e;\n    border-radius: 50%;\n  }\n  .eye::before {\n    content: \"\";\n    display: block;\n    border: 3px solid #000;\n    width: 31px;\n    height: 31px;\n    background: #fff;\n    border-radius: 50%;\n    left: 7px;\n    position: relative;\n  }\n  .eye.left {\n    transform: translateX(-100px);\n  }\n  .eye.right {\n    transform: translateX(100px);\n  }\n  .mouth {\n    width: 200px;\n    height: 200px;\n    position: absolute;\n    left: 50%;\n    top: 170px;\n    margin-left: -100px;\n    /* overflow: hidden; */\n  }\n  .mouth .up {\n    position: relative;\n    top: -20px;\n    z-index:1;\n  }\n  .mouth .up .lip {\n    border: 3px solid black;\n    background: #ffe600;\n    height: 30px;\n    width: 100px;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    position: relative;\n    position: absolute;\n    left: 50%;\n    margin-left: -50px;\n  }\n  .mouth .up .lip.left {\n    border-radius: 0 0 0 50px;\n    transform: rotate(-15deg) translateX(-53px);\n  }\n  .mouth .up .lip.right {\n    border-radius: 0 0 50px 0px;\n    transform: rotate(15deg) translateX(53px);\n  }\n  .mouth .up .lip::before {\n    content: \"\";\n    display: block;\n    width: 7px;\n    height: 30px;\n    position: absolute;\n    background: #ffe600;\n    bottom: 0;\n  }\n  .mouth .up .lip.left::before {\n    right: -6px;\n  }\n  .mouth .up .lip.right::before {\n    left: -6px;\n  }\n  .mouth .down {\n      height: 180px;\n      position: absolute;\n      top: 5px;\n      width: 100%;\n      overflow: hidden;\n  }\n  .mouth .down .yuan1{\n      border:3px solid black;\n      width: 150px;\n      height: 1000px;\n      position: absolute;\n      bottom: 0;\n      left: 50%;\n      margin-left: -75px;\n      border-radius: 75px/300px;\n      background: #9b000a;\n      overflow: hidden;\n  }\n  .mouth .down .yuan1 .yuan2{\n      width: 200px;\n      height: 300px;\n      position: absolute;\n      bottom: -155px;\n      left: 50%;\n      margin-left: -100px;\n      background: #ff485f;\n      border-radius: 100px;\n  }\n  .face{\n      position: absolute;\n      left: 50%;\n      border:3px solid black;\n      width: 88px;\n      height: 88px;\n      top: 200px;\n      margin-left: -44px;\n      z-index:3;\n  }\n  .face > img{\n      position: absolute;\n      top: 50%;\n      left: 50%;\n  }\n  .face.left {\n      transform: translateX(-180px);\n      background: #ff0000;\n      border-radius: 50%;\n  }\n  .face.left > img{\n      transform:rotateY(180deg);\n      transform-origin: 0 0;\n  }\n  .face.right{\n      transform: translateX(180px);\n      background: #ff0000;\n      border-radius: 50%;\n  }\n";
+exports.default = string;
+},{}],"main.js":[function(require,module,exports) {
+'use strict';
+
+var _css = require('./css.js');
+
+var _css2 = _interopRequireDefault(_css);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var player = {
+    id: undefined,
+    time: 100,
+    ui: {
+        demo: document.querySelector('#demo'),
+        demo2: document.querySelector('#demo2')
+    },
+    n: 1,
+    events: {
+        '#btnPause': 'pause',
+        '#btnPlay': 'play',
+        '#btnSlow': 'slow',
+        '#btnNormal': 'normal',
+        '#btnFast': 'fast'
+    },
+    init: function init() {
+        player.ui.demo.innerText = _css2.default.substr(0, player.n);
+        player.ui.demo2.innerHTML = _css2.default.substr(0, player.n);
+        player.bindEvents();
+        player.play();
+    },
+    bindEvents: function bindEvents() {
+        for (var key in player.events) {
+            if (player.events.hasOwnProperty(key)) {
+                var value = player.events[key];
+                document.querySelector(key).onclick = player[value];
+            }
+        }
+    },
+    run: function run() {
+        player.n += 1;
+        if (player.n > _css2.default.length) {
+            window.clearInterval(player.id);
+            return;
+        }
+        player.ui.demo.innerText = _css2.default.substr(0, player.n);
+        player.ui.demo2.innerHTML = _css2.default.substr(0, player.n);
+        player.ui.demo.scrollTop = player.ui.demo.scrollHeight;
+    },
+    play: function play() {
+        player.id = setInterval(player.run, player.time);
+    },
+    pause: function pause() {
+        window.clearInterval(player.id);
+    },
+    slow: function slow() {
+        player.pause();
+        player.time = 300;
+        player.play();
+    },
+    normal: function normal() {
+        player.pause();
+        player.time = 100;
+        player.play();
+    },
+    fast: function fast() {
+        player.pause();
+        player.time = 0;
+        player.play();
+    }
+};
+player.init();
+},{"./css.js":"css.js"}],"C:\\Users\\hmx\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -134,7 +211,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '2105' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '1687' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -276,3 +353,4 @@ function hmrAccept(bundle, id) {
   });
 }
 },{}]},{},["C:\\Users\\hmx\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.87e7d67a.map
